5b4397d2bb66542c607da2556e1b8b0906d4697b
Nklj <vlkvnklj@gmail.com>
2022-03-23 13:52:52 +0300

    change routes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Landing from "./Landing";
-import NotFound from "./NotFound";
 import App from "./App";
 
 const Router = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<Landing />} />
         <Route path="/restaurant/:restaurantId" element={<App />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        </Routes>
     </HashRouter>
   );
 };
