51f6c44d34ac7baf19e5ef29ad70c07f02d23752
Nklj <vlkvnklj@gmail.com>
2022-03-23 13:32:12 +0300

    change browserRouter on hashRouter

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Landing from "./Landing";
 import NotFound from "./NotFound";
 import App from "./App";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/restaurant/:restaurantId" element={<App />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
