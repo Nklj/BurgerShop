f71e543752b5e04375920a57c43bb78adbd2cb80
Nklj <vlkvnklj@gmail.com>
2022-03-23 13:09:22 +0300

    Updates

---
+++ precache-manifest.7437525b0bf3fc3e05869c5c32cf5083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214a75e767c57a2efc0793005ef722f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61928bd977d953a16a4b",
+    "url": "/static/css/main.cb412955.chunk.css"
+  },
+  {
+    "revision": "dfc9da2d0cd3c48b4ffd",
+    "url": "/static/js/2.473b80b2.chunk.js"
+  },
+  {
+    "revision": "61928bd977d953a16a4b",
+    "url": "/static/js/main.45c16632.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "45ab2799ba811ebd8effc6cefaead0e0",
+    "url": "/static/media/down.45ab2799.svg"
+  },
+  {
+    "revision": "2a5cfc3e6cfbfa4a29b113252299520c",
+    "url": "/static/media/up.2a5cfc3e.svg"
+  }
+]);
